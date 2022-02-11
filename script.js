07f00dffea94d8b7d96f2fe157d1fd2920db7a12
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-02-11 22:12:08 +0000

    Targeting html elements id + class (user input)

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// // Targeting html elements by id and class
+const locationInputEl = document.getElementById("locationInput");
+const button = document.getElementById("submit");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "8vWG87wRwlREjTnlTeKlyotzDgBt6A0G";
 const openWeatherAPIKey = "988fbbe10b9a8419e74f5e6d95338e7c";
