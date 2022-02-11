7fa5040b185989d556631db10f2545664a642118
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-02-11 22:16:17 +0000

    moment js used to use as a parameter for APIs

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const button = document.getElementById("submit");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "8vWG87wRwlREjTnlTeKlyotzDgBt6A0G";
 const openWeatherAPIKey = "988fbbe10b9a8419e74f5e6d95338e7c";
+// Retreiving then storing live date from moment js to specify the events displayed to be for on the day. Using the variable 'Today' as a paremeter in teh feth call to get events data.
+let Today = moment().format("YYYY-MM-DD");
 // Function to get events from ticket masters API, and then dynamically displaying data using cards. Function serves as a search handler.
 function searchHandler(e) {
   fetch(
