91d676fbcb4741aa6762a875d5fe076e19a99417
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-02-11 22:12:44 +0000

    Added event listener to search button

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const button = document.getElementById("submit");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "8vWG87wRwlREjTnlTeKlyotzDgBt6A0G";
 const openWeatherAPIKey = "988fbbe10b9a8419e74f5e6d95338e7c";
+// Search button event listener to run function searchHandler
+button.addEventListener("click", searchHandler);
