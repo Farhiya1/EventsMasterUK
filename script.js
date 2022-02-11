758d8b4102aadbd9aa7c9f97ceb56a946cb4f042
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-02-11 22:16:54 +0000

    targeting video on html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // // Targeting html elements by id and class
 const locationInputEl = document.getElementById("locationInput");
 const button = document.getElementById("submit");
+let video = document.querySelector("video");
 // Storing Api Keys in const
 const ticketMasterAPIKey = "8vWG87wRwlREjTnlTeKlyotzDgBt6A0G";
 const openWeatherAPIKey = "988fbbe10b9a8419e74f5e6d95338e7c";
